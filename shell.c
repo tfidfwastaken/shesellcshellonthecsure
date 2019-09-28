33bf730dd6e5bc6a88a40eb5c66178e3ef90b61e
Atharva Raykar <raykar.ath@gmail.com>
2019-09-28 17:48:00 +0530

    Add parsing and tokenizing

--- shell.c
+++ shell.c
@@ -20,6 +20,45 @@ void shloop(void)
     } while(status);
 }
 
+char *sh_read_line()
+{
+    char *line = NULL;
+    ssize_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+char **sh_split_line(char *line)
+{
+    size_t bufsize = SH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(sizeof(char) * bufsize);
+    char *token;
+
+    if(!tokens) {
+        fprintf(stderr, "she sell: allocation error!");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SH_TOK_DELIMS);
+    while(token != NULL) {
+        token[position] = token;
+        position++;
+
+        if(position >= bufsize) {
+            bufsize += SH_TOK_DELIMS;
+            tokens = realloc(tokens, sizeof(char) * bufsize);
+
+            if(!tokens) {
+                fprintf(stderr, "she sell: allocation error!");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    token = strtok(NULL, SH_TOK_DELIMS);
+    return tokens;
+}
+
 uint8_t main(int argc, char **argv)
 {
     //Load config
--- shell.h
+++ shell.h
@@ -1,4 +1,8 @@
 #define EXIT_SUCCESS 0
+#define EXIT_FAILURE -1
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIMS "\t\r\n\a"
 
 void shloop(void);
-char *sh_read()
+char *sh_read_line();
+char **sh_split_line(char *line);
